667bfed3064502378191f2f098d0e46c46eebdee
Shawn Waldon <shawn.waldon@kitware.com>
2016-04-15 10:47:26 -0400

    Fix itk install script to place python libs correctly

--- projects/itk.install.cmake
+++ projects/itk.install.cmake
@@ -1,5 +1,14 @@
-foreach (dir bin include lib share)
+foreach (dir bin include share)
   file(GLOB files "${dir}/*")
   file(INSTALL ${files}
        DESTINATION "${install_dir}/${dir}")
 endforeach()
+
+file(GLOB libfiles "lib/*")
+file(INSTALL ${libfiles}
+  DESTINATION "${install_dir}/lib"
+  FILES_MATCHING REGEX "python2.7" EXCLUDE)
+
+file(GLOB pyfiles "lib/itk/python2.7/*")
+file(INSTALL ${pyfiles}
+  DESTINATION "${install_dir}/lib/python2.7")
